25b9127748dce63d991682172cdbc3eefe234ed4
Lawrence Cheung <lcheung@lasallecollegevancouver.com>
2017-08-29 19:29:05 -0700

    add vs project
    Add Visual Studio Unit Test porject

---
+++ demoUnitTest/demoUnitTest/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace demoUnitTest
+{
+    public class StringCalculatorTest
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.AreEqual(0, 0);
+        }
+    }
+}
